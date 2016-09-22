7c57cb53d3fadc486b6c25c384d28a0f2ee71c1a
Zortrox <zortroxofdoom@gmail.com>
2016-09-22 12:16:25 -0500

    made history work

--- main.c
+++ main.c
@@ -53,6 +53,63 @@ void intHandler(int sig) {
 	signal(sig, intHandler);
 }
 
+bool isHistoryComm(char* cArgs[MAX_ARGS]) {
+	return (cArgs[0] != NULL && strcmp(cArgs[0], "r") == 0);
+}
+
+void parseInput(char* input, char* inputSplit, char* cArgs[MAX_ARGS], bool* bRunConcurr, bool original) {
+	bool goodInput = true;
+
+	if (!original) goodInput = false;
+
+	//if the input captured the Crtl+C signal, remove and shift string
+	if (input[0] == '\xFF') {
+		int size = sizeof(input) / sizeof(char);
+		for (int i = 0; i < size - 1; i++) {
+			if (input[i] != '\0') {
+				input[i] = input[i + 1];
+			}
+			else {
+				break;
+			}
+		}
+	}
+
+	strcpy(inputSplit, input);
+
+	int argIndex = 0;
+	char* argPtr;
+	argPtr = strtok(inputSplit, " ");
+	while (argPtr != NULL && argIndex < MAX_ARGS - 1)
+	{
+		cArgs[argIndex] = argPtr;
+		argPtr = strtok(NULL, " ");
+		argIndex++;
+	}
+	cArgs[argIndex] = NULL;	//for end of arguments
+
+	if (isHistoryComm(cArgs)) goodInput = false;
+
+	//determine if command will run concurrently
+	//then remove from arguments
+	for (int i = 0; i < MAX_ARGS; i++) {
+		if (cArgs[i] == NULL) {
+			if (i > 0 && strcmp(cArgs[i-1], "&") == 0) {
+				*bRunConcurr = true;
+				cArgs[i-1] = NULL;
+			}
+			else if (i == 0) {
+				goodInput = false;
+			}
+			break;
+		}
+	}
+
+	if (goodInput) {
+		insertVector(&vecComms, input);
+	}
+}
+
 int main(int argc, const char* argv[]) {
 
 	signal(SIGINT, intHandler);
@@ -66,6 +123,7 @@ int main(int argc, const char* argv[]) {
 		char c;
 		int index = 0;
 		printf("sh> ");
+		fflush(stdin);
 		while ((c = getchar()) != '\n') {
 			int length = sizeof(input) / sizeof(input[0]) - 1;	//save 1 for ending character
 			if (index < length) {
@@ -77,55 +135,30 @@ int main(int argc, const char* argv[]) {
 		while (c != '\n' && c != EOF) {
 			c = getchar();
 		}
-
-		if (input[0] == '\xFF') {
-			int size = sizeof(input) / sizeof(char);
-			for (int i = 0; i < size - 1; i++) {
-				if (input[i] != '\0') {
-					input[i] = input[i + 1];
-				}
-				else {
-					break;
-				}
-			}
-		}
-		char inputSplit[sizeof(input) * sizeof(char)];
-		strcpy(inputSplit, input);
-
-		int argIndex = 0;
-		char* argPtr;
-		argPtr = strtok(inputSplit, " ");
-		while (argPtr != NULL && index < MAX_ARGS - 1)
-		{
-			cArgs[argIndex] = argPtr;
-			argPtr = strtok(NULL, " ");
-			argIndex++;
-		}
-		cArgs[argIndex] = NULL;	//for end of arguments
-
-		//determine if command will run concurrently
-		//then remove from arguments
+		
 		bool bRunConcurr = false;
-		for (int i = 0; i < MAX_ARGS; i++) {
-			if (cArgs[i] == NULL) {
-				if (strcmp(cArgs[i - 1], "&") == 0) {
-					bRunConcurr = true;
-					cArgs[i - 1] = NULL;
-				}
-				break;
+		char inputSplit[sizeof(input) * sizeof(char)];
+		parseInput(input, inputSplit, cArgs, &bRunConcurr, true);
+
+		//run a previous command
+		if (isHistoryComm(cArgs)) {
+			long num = strtol(cArgs[1], NULL, 10);
+			if (num <= vecComms.size && num > 0) {
+				char* comm = vecComms.vector[num - 1];
+
+				char commSplit[sizeof(input) * sizeof(char)];
+				parseInput(comm, commSplit, cArgs, &bRunConcurr, false);
+			}
+			else {
+				printf("Command not found\n");
 			}
 		}
-
-
-
-		insertVector(&vecComms, input);
-
 
 		int pid = fork();
 		//child
 		if (pid == 0) {
+
 			execvp(cArgs[0], cArgs);
-			_exit(0);
 		}
 		//parent
 		else if (pid > 0 && !bRunConcurr) {
